bb7a31f6b4a25a0efd9941dd48177ca6f1487637
hes0sss <cusnircristi161@gmail.com>
2025-02-19 14:31:35 +0200

    i swiched database type to mysql for my comfotability and i now use railway for hosting database and backend server (maybe there will be also hosted vue app

---
+++ web_news/backend/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use (cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+
+db.connect((err) => {
+    if(err){
+        console.log(err);
+    }
+    console.log('Database connected!');
+});
+
+
+app.get('/news', (req, res) => {
+    db.query("SELECT * FROM news", (err, result) => {
+        if(err) return console.status(500).json({err});
+        res.json(result);
+    });
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
