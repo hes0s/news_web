da9bdc656b26af1946a1cb85d5c74901d04e7263
hes0sss <cusnircristi161@gmail.com>
2025-02-19 13:18:52 +0200

    from 0 on the database topic

--- web_news/news_vue_new/vue-webapp/src/views/grid_news.vue
+++ web_news/news_vue_new/vue-webapp/src/views/grid_news.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="all">
     <!-- Highlight the Latest News -->
-    <div v-if="items.length > 0" class="card mb-3 mx-auto mt-5" style="width: 800px; height: 500px">
-      <img :src="items[0].photo" class="card-img-top" alt="Latest News Photo" />
+    <div  class="card mb-3 mx-auto mt-5" style="width: 800px; height: 500px">
+      <img src="aboba.jpg" class="card-img-top" alt="Latest News Photo" />
       <div class="card-body">
         <h4 class="card-sub-title">Noutatea zilei</h4>
-        <h5 class="card-title">{{ items[0].name }}</h5>
-        <p class="card-text">{{ items[0].description }}</p>
+        <h5 class="card-title"></h5>
+        <p class="card-text"></p>
         <p class="card-text"><small class="text-body-secondary">Last updated just now</small></p>
       </div>
     </div>
 
     <!-- Grid of Other News -->
     <div class="row row-cols-1 row-cols-md-3 g-4 mx-auto" style="width: 800px">
-      <div v-for="(news, index) in items.slice(1)" :key="index" class="col">
+      <div class="col">
         <div class="card h-100">
           <img :src="news.photo" class="card-img-top" alt="News Photo" />
           <div class="card-body">
-            <h5 class="card-title">{{ news.name }}</h5>
-            <p class="card-text">{{ news.description }}</p>
+            <h5 class="card-title"></h5>
+            <p class="card-text"></p>
           </div>
         </div>
       </div>
@@ -27,26 +27,9 @@
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
-import axios from "axios";
-
 export default {
   name: "GridNews",
-  
-  data() {
-    return {
-      newsList: [],
-    };
-  },
-
-  methods: {
-    async fetchNews() {
-      const response = await axios.get("http://localhost:3000/news");
-      this.newsList = response.data;
-    },
-  },
-
-  mounted() {
+mounted() {
     this.fetchNews();
   },
 };
