3e1ad59da550676b3f4536a1376cb50aed2823de
hes0sss <cusnircristi161@gmail.com>
2025-02-20 20:11:43 +0200

    ??

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,13 @@ import dotenv from 'dotenv';
 //set up express
 dotenv.config();
 const app = express();
-app.use(cors({ 
-    origin: "*", // ✅ Allow your Netlify frontend
-    methods: "GET,POST,PUT,DELETE", 
-    credentials: true 
-  }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://rainbow-cuchufli-46a42d.netlify.app");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
   
 
 const db = mysql.createConnection({
