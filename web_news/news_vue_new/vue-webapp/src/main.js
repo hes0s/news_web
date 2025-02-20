884be3a5e5f5141ca637f080c37e797a0f83df74
hes0sss <cusnircristi161@gmail.com>
2025-02-20 19:36:40 +0200

    jdn

--- web_news/news_vue_new/vue-webapp/src/main.js
+++ web_news/news_vue_new/vue-webapp/src/main.js
@@ -2,10 +2,11 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
-
+import axios from 'axios'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+app.config.globalProperties.$axios = axios;
 app.config.devtools = true; // Force enable Vue DevTools
 console.log('App loaded!')
 
